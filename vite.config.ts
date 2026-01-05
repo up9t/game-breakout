6628a0569c1e055c0beed8a7fa828e28a51904db
ilyasa1211 <muhammadilyasafadhlih@gmail.com>
2026-01-05 03:37:22 +0000

    Remove unused dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+// import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig((env) => ({
   plugins: [
     tailwindcss(),
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
+    // tanstackRouter({
+    //   target: "react",
+    //   autoCodeSplitting: true,
+    // }),
     preact(),
   ],
   base: env.mode === "github" ? "/game-breakout" : "/",
