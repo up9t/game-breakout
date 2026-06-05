0e93a8707d2a76b753c142696cdbe340f7f03332
up9t <up9t99@gmail.com>
2026-06-05 19:27:22 +0700

    Fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig((env) => ({
   plugins: [
     tailwindcss(),
-    // tanstackRouter({
-    //   target: "react",
-    //   autoCodeSplitting: true,
-    // }),
     preact(),
   ],
   base: env.mode === "github" ? "/game-breakout" : "/",
